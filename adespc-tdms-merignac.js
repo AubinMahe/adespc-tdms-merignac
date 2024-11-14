e446b1ed6f6a3df9c0eb27a02721d1980dd0ce82
Aubin <aubin.mahe@wanadoo.fr>
2024-11-14 18:23:28 +0100

    Correction 13e mois des mensuels

--- adespc-tdms-merignac.js
+++ adespc-tdms-merignac.js
@@ -1,4 +1,5 @@
 function adespc_tdms_merignac() {
+   this.TALON_2024    = 2_450.00;
    this.salary_ui     = document.getElementById("salary");
    this.position_ui   = document.getElementById("position");
    this.line_ui       = document.getElementById("line");
@@ -15,9 +16,21 @@ adespc_tdms_merignac.prototype.add_event_handlers = function() {
    this.position_ui.addEventListener( 'change', e => this.onPositionChange( e ));
 }
 
-adespc_tdms_merignac.prototype.onSalaryChange = function( e ) {
-   const value = parseFloat( this.salary_ui.value );
+adespc_tdms_merignac.prototype.update = function( e ) {
+   const position = this.position_ui.value;
+   let value = parseFloat( this.salary_ui.value );
    if( value > 1_999.99 && value < 14_000.01 ) {
+      if( position.substring( 0, 1 ) < 'F' ) {
+         // Prise en compte du 13e mois pour les mensuel
+         if( value < this.TALON_2024 ) {
+            // 13e mois minimum
+            value += this.TALON_2024 / 12.0;
+         }
+         else {
+            // 13e mois calculé
+            value *= 13.00 / 12.00;
+         }
+      }
       const y = this.line_2000 - this.line_scale * ( value - 2_000.0 );
       this.line_ui.style.top = y + "px";
       this.line_ui.style.display = 'block';
@@ -25,23 +38,21 @@ adespc_tdms_merignac.prototype.onSalaryChange = function( e ) {
    else {
       this.line_ui.style.display = 'none';
    }
+   const column_width = 92.5;
+   const ndx          = parseInt( position.substring( 1 )) - 5;
+   this.left_mask_ui .style.display = ( position == "C5" )? "none" : "block";
+   this.left_mask_ui .style.width   = (     ndx*column_width) + "px";
+   this.right_mask_ui.style.display = ( position == "H16" )? "none" : "block";
+   this.right_mask_ui.style.left    = ( 118+(ndx+1)*column_width) + "px";
+   this.right_mask_ui.style.width   = (1015-ndx*column_width) + "px";
+}
+
+adespc_tdms_merignac.prototype.onSalaryChange = function( e ) {
+   this.update();
 }
 
 adespc_tdms_merignac.prototype.onPositionChange = function( e ) {
-   const position = this.position_ui.value;
-   if( position == "ALL" ) {
-      this.left_mask_ui .style.display = "none";
-      this.right_mask_ui.style.display = "none";
-   }
-   else {
-      const column_width = 92.5;
-      const ndx          = parseInt( position.substring( 1 )) - 5;
-      this.left_mask_ui .style.display = ( position == "C5" )? "none" : "block";
-      this.left_mask_ui .style.width   = (     ndx*column_width) + "px";
-      this.right_mask_ui.style.display = ( position == "H16" )? "none" : "block";
-      this.right_mask_ui.style.left    = ( 118+(ndx+1)*column_width) + "px";
-      this.right_mask_ui.style.width   = (1015-ndx*column_width) + "px";
-   }
+   this.update();
 }
 
 new adespc_tdms_merignac();
