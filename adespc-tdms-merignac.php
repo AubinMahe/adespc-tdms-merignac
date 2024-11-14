dae59b2f1481e9048a94441e5b499d578af2a657
Aubin <aubin.mahe@wanadoo.fr>
2024-11-14 08:05:17 +0100

    Création du plugin WordPress

---
+++ adespc-tdms-merignac.php
@@ -0,0 +1,21 @@
+/**
+ * adespc-tdms-merignac
+ *
+ * @package           adespc-tdms-merignac
+ * @author            Aubin MAHÉ
+ * @copyright         2024 Aubin MAHÉ
+ * @license           LGPL-3.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       adespc-tdms-merignac
+ * Plugin URI:        https://github.com/AubinMahe/adespc-tdms-merignac
+ * Description:       Ce plugin affiche les catégories de la nouvelle CCNM en abscisse et les salaires bruts mensuels en ordonné, sollicite l'utilisateur pour qu'il communique son salaire et sa classification puis le localise graphiquement.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Aubin MAHÉ
+ * Author URI:        https://github.com/AubinMahe/adespc-tdms-merignac
+ * Text Domain:       CGT
+ * License:           LGPL-3.0
+ * License URI:       https://www.gnu.org/licenses/lgpl-3.0.txt
+ */
